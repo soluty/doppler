9085adebece0faaf2c36824a8264418e4afce916
soluty <soluty.lv@qq.com>
2025-12-10 15:56:01 +0800

    p21

--- apps/p2/index.js
+++ apps/p2/index.js
@@ -1,3 +1,4 @@
-console.log('=== Deploying P2 ===');
-console.log('VALUE_P2:', process.env.VALUE_P2 || 'Not set');
-console.log('Deployment completed for P2 at', new Date().toISOString());
+console.log('=== Deploying P2 ===1')
+console.log('VALUE_P2:', process.env.VALUE_P2 || 'Not set')
+console.log('Deployment completed for P2 at', new Date().toISOString())
+
